24cc4da216340244fc73267c424548ffb3f18dc2
svdevops001 <93402966+svdevops001@users.noreply.github.com>
2021-12-01 03:50:35 +0530

    Update 10-aks-cluster-windows-user-nodepools.tf

--- terraform-manifests/10-aks-cluster-windows-user-nodepools.tf
+++ terraform-manifests/10-aks-cluster-windows-user-nodepools.tf
@@ -2,14 +2,14 @@
 /*
 resource "azurerm_kubernetes_cluster_node_pool" "win101" {
   availability_zones    = [1, 2, 3]
-  enable_auto_scaling   = true
+  enable_auto_scaling   = false
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
-  max_count             = 3
+  max_count             = 1
   min_count             = 1
   mode                  = "User"
   name                  = "win101"
   orchestrator_version  = data.azurerm_kubernetes_service_versions.current.latest_version
-  os_disk_size_gb       = 30
+  os_disk_size_gb       = 10
   os_type               = "Windows" # Default is Linux, we can change to Windows
   vm_size               = "Standard_DS2_v2"
   priority              = "Regular"  # Default is Regular, we can change to Spot with additional settings like eviction_policy, spot_max_price, node_labels and node_taints
